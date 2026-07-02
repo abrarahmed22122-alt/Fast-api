a006ae03b251c77875b533a8202e734e871146bf
abrarahmed22122-alt <abrarahmed22122@gmail.com>
2026-07-02 16:54:37 +0530

    Uncomment FastAPI routes and related imports

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
-# from fastapi import FastAPI
-# app = FastAPI()
+ from fastapi import FastAPI
+ app = FastAPI()
 
-# @app.get("/")
-# def home():
-#     return {
-#         "Message":"pytest"
-#     }
+@app.get("/")
+def home():
+    return {
+        "Message":"pytest"
+    }
 
-# @app.get("/add")
-# def add(a:int,b:int):
-#     return {
-#         "result":a+b
-#     }
+@app.get("/add")
+def add(a:int,b:int):
+    return {
+        "result":a+b
+    }
 
-# from fastapi import FastAPI
-# import requests
+from fastapi import FastAPI
+import requests
 
-# app = FastAPI()
+app = FastAPI()
 
-# @app.get("/posts")
-# def get_posts():
-#     url = "https://jsonplaceholder.typicode.com/posts"
-#     response = requests.get(url)
-#     return response.json()
+@app.get("/posts")
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    return response.json()
 
-# #Get Single Post
-# @app.get("/posts/{post_id}")
-# def get_post(post_id:int):
-#     url = "https://jsonplaceholder.typicode.com/posts"
-#     response = requests.get(url)
-#     return response.json()
+#Get Single Post
+@app.get("/posts/{post_id}")
+def get_post(post_id:int):
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    return response.json()
 
-# from fastapi import FastAPI
-# import requests
-# from bs4 import BeautifulSoup
+from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
 
-# app = FastAPI()
-# @app.get("/News")
-# def get_news():
-#     url = "https://news.google.com/home?hl=en-IN&gl=IN&ceid=IN%3Aen"
-#     response = requests.get(url)
-#     soup = BeautifulSoup(response.text,"html.parser")
+app = FastAPI()
+@app.get("/News")
+def get_news():
+    url = "https://news.google.com/home?hl=en-IN&gl=IN&ceid=IN%3Aen"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,"html.parser")
 
-#     title = []
+    title = []
 
-#     for items in soup.find_all("a",class_ = "WwrzSb"):
-#         title.append(items.text)
+    for items in soup.find_all("a",class_ = "WwrzSb"):
+        title.append(items.text)
 
-#     return{
-#         "News":title
-#     }
+    return{
+        "News":title
+    }
 
 
 from fastapi import FastAPI, Request
