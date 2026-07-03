ed6d0991deeac17cce749f3645636cb9d9cbdef1
abrar ahmed <abrarahmed22122@gmail.com>
2026-07-03 10:08:09 +0530

    Upload main.py

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def home():
     return {
         "Message":"WELCOME TO FASTAPI"
     }
+
+#About route
+@app.get("/about")
+def about():
+    return {
+        "Message":"This is about page"
+    }
