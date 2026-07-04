e2262e67b46b91a68d5a219416892492b4444d9c
abrar ahmed <abrarahmed22122@gmail.com>
2026-07-04 11:28:09 +0530

    Upload main3.py

--- main3.py
+++ main3.py
@@ -2,7 +2,19 @@
 #REQUEST BODY,JSON INPUT HANDLING, POST REQUEST,INTRO TO PYDANTIC
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class User(BaseModel):
+    name : str
+    age : int
+@app.post("/createuser")
+def createuser(user:User):
+    return{
+    "Message":"user created",
+    "data" : user
+    }
+
 
 @app.post("/create_user")
 def create_user(name:str,age:int):
@@ -10,3 +22,6 @@ def create_user(name:str,age:int):
         "Name" : name,
         "Age" : age
     }
+
+
+
