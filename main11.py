1dcdda93babc451ab7500c77795dc72594b272f9
abrar ahmed <abrarahmed22122@gmail.com>
2026-07-13 15:32:14 +0530

    Upload main11.py

---
+++ main11.py
@@ -0,0 +1,22 @@
+#LECTURE 12
+#MIDDLEWARE, LOGGING MIDDLEWARE, REQUEST RESPONSE FLOW
+
+from fastapi import FastAPI, Request
+import time
+app = FastAPI()
+
+@app.middleware("http")
+async def my_middleware(request:Request,call_next):
+    print("Request Received")
+
+    response = await call_next(request)
+    print("Response Sent")
+    return response
+
+@app.middleware("http")
+async def log_middleware(request:Request,call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"path{request.url.path} | Time : {process_time}")
+    return response
