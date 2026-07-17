3c510e40ba8dcd677601455158900ffac79e6b49
abrar ahmed <abrarahmed22122@gmail.com>
2026-07-17 18:46:21 +0530

    Upload dsa.py

--- dsa.py
+++ dsa.py
@@ -1,4 +1,12 @@
-print(2==2)
-print(2+2)
-print("abrar")
-print("brar")
+for i in range(1,6):
+    for j in range(1,6-i):   
+        print(" ", end=" ")
+    for k in range(1,i*2):
+        print(k,end = " ")
+    print()
+for i in range(4,0,-1):
+    for j in range(1,6-i):   
+        print(" ", end=" ")
+    for k in range(1,i*2):
+        print(k,end = " ")
+    print()
